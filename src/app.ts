8bdf3173e33e090d1784d39ada81bc133bfa77bf
jordy repi <joju19grifith@gmail.com>
2024-05-26 14:57:19 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,62 @@
+import cors from "cors";
+
+import express, {
+  Express,
+  json,
+  urlencoded,
+  static as static_,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+import { join } from "path";
+import { PORT } from "./config";
+
+export default class App {
+  private app: Express;
+
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.routes();
+    this.handleError();
+  }
+
+  private configure(): void {
+    this.app.use(cors());
+    this.app.use(json());
+    this.app.use(urlencoded({ extended: true }));
+    this.app.use("/", static_(join(__dirname, "../public")));
+  }
+
+  private handleError(): void {
+    // not found
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      if (req.path.includes("/api/")) {
+        res.status(404).send("Not found !");
+      } else {
+        next();
+      }
+    });
+
+    // error
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (req.path.includes("/api/")) {
+          console.error("Error : ", err.stack);
+          res.status(500).send(err.message);
+        } else {
+          next();
+        }
+      }
+    );
+  }
+
+  private routes(): void {}
+
+  public start(): void {
+    this.app.listen(PORT, () => {
+      console.log(`  ➜  [API] Local:   http://localhost:${PORT}/`);
+    });
+  }
+}
